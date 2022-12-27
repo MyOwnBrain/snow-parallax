2fa0ea11a52502a1def4d6ddeb595476f3111390
MyOwnBrain <81764985+MyOwnBrain@users.noreply.github.com>
2022-12-27 14:17:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const body = document.body;
-const background = document.getElementById("background")
-const snow = document.getElementById("snow")
-const trees = document.getElementById("trees")
+const background = document.getElementById("background");
+const snow = document.getElementById("snow");
+const trees = document.getElementById("trees");
 
-let element_list = [background, ...snow.children, trees].reverse()
+let element_list = [background, ...snow.children, trees].reverse();
 
 body.addEventListener("mousemove", (e) => {
     element_list.forEach((element, index) => {
@@ -12,6 +12,6 @@ body.addEventListener("mousemove", (e) => {
         let width_to_percent = parseFloat(getComputedStyle(body).width) / 100;
         let percent_to_moveby = ((e.pageX / width_to_percent) * (max / 100)) - (max / 2);
     
-        element.style.transform = `translateX(${percent_to_moveby / layer}px)`
+        element.style.transform = `translateX(${percent_to_moveby / layer}px)`;
     })
 })
