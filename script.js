0771b6bb8adace2b63d4769c735857dfa7b00654
MyOwnBrain <81764985+MyOwnBrain@users.noreply.github.com>
2022-12-27 13:42:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const body = document.body;
+const background = document.getElementById("background")
+const snow = document.getElementById("snow")
+const trees = document.getElementById("trees")
+
+let element_list = [background, ...snow.children, trees].reverse()
+
+body.addEventListener("mousemove", (e) => {
+    element_list.forEach((element, index) => {
+        let layer = (index + 1) / 2;
+        let max = 250;
+        let width_to_percent = parseFloat(getComputedStyle(body).width) / 100;
+        let percent_to_moveby = ((e.pageX / width_to_percent) * (max / 100)) - (max / 2);
+    
+        element.style.transform = `translateX(${percent_to_moveby / layer}px)`
+    })
+})
